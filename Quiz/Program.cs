34c873e095a461017432d4daead129cb585c6b62
wesselottevanger <wesselott2@gmal.com>
2017-11-18 16:52:24 +0100

    Vraag Klasse gemaakt en vragen bedacht

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -3,10 +3,45 @@
 namespace Quiz
 {
     class Program
-    {
+    { 
+        int answerscorrect = 0;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Vraag vraag1 = new Vraag()
+            {
+                inhoud = "wat is 11 x 23",
+                antwoord = "253"
+            };
+            Vraag vraag2 = new Vraag()
+            {
+                inhoud = "welke kleur is een schaap?",
+                antwoord = "Wit" 
+            };
+            Vraag vraag3 = new Vraag()
+            {
+                inhoud = "Zijn alle insecten groen?" +
+            "A: Ja" +
+            "B: Nee",
+                antwoord = "B"
+            };
+            Vraag vraag4 = new Vraag()
+            {
+                inhoud = "Kunnen alle vissen zwemmen?" +
+            "A: Ja" +
+            "B: Nee",
+                antwoord = "A"
+            };
+            Vraag vraag5 = new Vraag()
+            {
+                inhoud = "Hoeveel minuten zitten er in een dag?",
+                antwoord = "1440"
+            };
+            Vraag vraag6 = new Vraag()
+            {
+                inhoud = "Welke kleur is een kameleon",
+                antwoord = "elke"
+            };
         }
     }
 }
---
+++ Quiz/Vraag.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quiz
+{
+    public class Vraag
+    {
+        public string inhoud { get; set; }
+        public string antwoord { get; set; }
+    }
+}
